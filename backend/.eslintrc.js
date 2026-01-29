d3478f89026607799bdc94d25e2cd4e58b323971
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-29 13:54:37 +0530

    chore(backend): add ESLint configuration

---
+++ backend/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'object-shorthand': 'error',
+    'prefer-template': 'error',
+  },
+  overrides: [
+    {
+      files: ['tests/**/*.js', '**/*.test.js', '**/*.spec.js'],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
