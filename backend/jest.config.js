4f062a77bb2186fedbeacdb6a55786a6117e994c
Narvin Sachaniya <githubnarvin@gmail.com>
2026-01-29 13:55:20 +0530

    test(backend): add Jest configuration for testing

---
+++ backend/jest.config.js
@@ -0,0 +1,32 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+
+module.exports = {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testMatch: [
+    '**/tests/**/*.test.js',
+    '**/__tests__/**/*.js',
+    '**/?(*.)+(spec|test).js'
+  ],
+  collectCoverageFrom: [
+    'controllers/**/*.js',
+    'models/**/*.js',
+    'routes/**/*.js',
+    'middleware/**/*.js',
+    'utils/**/*.js',
+    '!**/node_modules/**',
+    '!**/tests/**',
+    '!**/coverage/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  testTimeout: 30000
+};
